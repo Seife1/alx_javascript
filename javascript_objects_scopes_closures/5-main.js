e309702d630a18d8d5775f57f3f6725b3fe9ec1b
Seife1 <gebrealabate19@yahoo.com>
2023-09-07 22:50:34 +0300

    fields

--- javascript_objects_scopes_closures/5-main.js
+++ javascript_objects_scopes_closures/5-main.js
@@ -1,6 +1,7 @@
 const Square = require('./5-square');
 
 const s1 = new Square(4);
+console.log(s1);
 s1.print();
 s1.double();
 s1.print();
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 // a class Square that defines a square and inherits from Square of 5-square.js
 
-const Square1 = require('./5-square.js');
+const Square = require('./5-square.js');
 
-class Square extends Square1 {
-  constructor (size) {
-    super (size);
-  }
+class SquareE extends Square {
 
   charPrint(c) {
-    for (let i = 1; i <= this.size; i++) {
+    for (let i = 1; i <= this.height; i++) {
         let output = '';
-        for (let j = 1; j <= this.size; j++) {
+        for (let j = 1; j <= this.width; j++) {
             if (c === undefined) {
               output += 'X';
             } else {
@@ -23,4 +20,4 @@ class Square extends Square1 {
   }
 }
 
-module.exports = Square;
+module.exports = SquareE;
