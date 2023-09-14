a27c6f5f7c1284f2fa21cd8928149aa4dd4f7ea1
Seife1 <gebrealabate19@yahoo.com>
2023-09-14 05:14:45 +0300

    err js

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -10,4 +10,7 @@ const url = `${process.argv[2]}`;
 request.get(url)
   .on('response', function (response) {
     console.log('Code:', response.statusCode);
-  });
+  })
+  .on('error', function (err) {
+    console.log(err);
+  })
