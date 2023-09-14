0814537a33e327a83c28d79489add11ef70e826d
Seife1 <gebrealabate19@yahoo.com>
2023-09-14 04:59:38 +0300

    JS ntw

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/***
+  A script that display the status code of a GET request.
+***/
+
+const request = require('request');
+const url = `${process.argv[2]}`;
+
+request.get(url)
+  .on('response', function (response) {
+    console.log('Code:', response.statusCode);
+  });
