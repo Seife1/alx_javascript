9419eaa910fc0baf29a7807386e5c1d21b2080fc
Seife1 <gebrealabate19@yahoo.com>
2023-09-14 05:44:03 +0300

    stderr

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -7,7 +7,12 @@
 const request = require('request');
 const url = `${process.argv[2]}`;
 
+const parts = [];
+
 request.get(url)
+  .on('data', function (data) {
+    parts.push(data);
+  })
   .on('response', function (response) {
     console.log('Code:', response.statusCode);
   })
